fbcf32343814c4dd50f0e366930633f52f3e84a6
jpbaldon <jonathan.baldon@gmail.com>
2025-06-09 14:17:43 -0400

    Project Cleanup
    Moved release contents into build and deleted irrelevant folders/files in build

--- hybrid_app/build/.qt/QtDeployTargets-Debug.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_HybridApp_FILE C:/Users/jonat/Documents/GitHub/SquidstatProject/hybrid_app/build/cpp_gui/Debug/HybridApp.exe)
-set(__QT_DEPLOY_TARGET_HybridApp_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_HybridApp_RUNTIME_DLLS C:/Qt/6.9.1/msvc2022_64/bin/Qt6Chartsd.dll;C:/Qt/6.9.1/msvc2022_64/bin/Qt6OpenGLWidgetsd.dll;C:/Qt/6.9.1/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.9.1/msvc2022_64/bin/Qt6OpenGLd.dll;C:/Qt/6.9.1/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.9.1/msvc2022_64/bin/Qt6Cored.dll)
--- hybrid_app/build/.qt/QtDeployTargets-Release.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_HybridApp_FILE C:/Users/jonat/Documents/GitHub/SquidstatProject/hybrid_app/build/cpp_gui/Release/HybridApp.exe)
-set(__QT_DEPLOY_TARGET_HybridApp_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_HybridApp_RUNTIME_DLLS C:/Qt/6.9.1/msvc2022_64/bin/Qt6Charts.dll;C:/Qt/6.9.1/msvc2022_64/bin/Qt6OpenGLWidgets.dll;C:/Qt/6.9.1/msvc2022_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/msvc2022_64/bin/Qt6OpenGL.dll;C:/Qt/6.9.1/msvc2022_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/msvc2022_64/bin/Qt6Core.dll)
